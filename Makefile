bb92e7bd28a1c5e993799b566b2bfe3e146cab3d
Alexey Solovyev <solovyev.alexey@gmail.com>
2024-08-22 20:36:54 -0600

    Create test_client.ml

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-all: server.cmo
+all: server.cmo test_client.cmo
 
 server.cmo: server.ml
 	ocamlc -c -I +compiler-libs unix.cma server.ml
+
+test_client.cmo: test_client.ml
+	ocamlc -c unix.cma test_client.ml
 
 clean:
 	rm *.cmi *.cmo
---
+++ test_client.ml
@@ -0,0 +1,26 @@
+let get_sockaddr host_name port =
+  let host = Unix.gethostbyname host_name in
+  let address = host.h_addr_list.(0) in
+  Unix.ADDR_INET (address, port)
+
+let client ?(host_name = "localhost") port =
+  Sys.catch_break true;
+  let addr = get_sockaddr host_name port in
+  let in_chan, out_chan = Unix.open_connection addr in
+  try
+    while true do
+      let line = input_line in_chan in
+      Printf.printf "%s\n%!" line;
+      if line = "ready" then begin
+          let cmd = input_line stdin in
+          output_string out_chan cmd;
+          output_string out_chan "\n";
+          flush out_chan
+      end
+    done
+  with exn ->
+    Unix.shutdown_connection in_chan;
+    close_out out_chan;
+    raise exn
+
+let () = client 2012
