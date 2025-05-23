21bc8400dd04c41b1edd16a6f7956db0c28c6cd6
Juneyoung Lee <aqjune@gmail.com>
2025-05-23 13:31:01 -0500

    Add unset/set_jrh_lexer to server2.ml. server2.ml can only be loaded, not compiled

--- server2.ml
+++ server2.ml
@@ -1,3 +1,5 @@
+unset_jrh_lexer;;
+
 exception Timeout
 exception Sigpipe
 
@@ -281,4 +283,6 @@ let start ?single_connection ?(host_name = "127.0.0.1") port =
   Format.printf "MT Server; PID: %d; Host address: %s; port number: %d (no forks)@." 
     $ Unix.getpid () $ Unix.string_of_inet_addr address $ port;
   flush_all();
-  establish_forkless_server ?single_connection mt_service (Unix.ADDR_INET (address, port))
+  establish_forkless_server ?single_connection mt_service (Unix.ADDR_INET (address, port));;
+
+set_jrh_lexer;;
